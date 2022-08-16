6558a55c99f3b3713ca032f6d274f039a0331498
Breno Nascimento <satsukebare23@gmail.com>
2022-08-16 12:22:12 -0300

    add color transition in header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,22 +1,56 @@
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
-import React from 'react'
 import style from './Header.module.scss'
 
 function Header() {
+  const [scrollY, setScrollY] = useState(0);
+  const verifyY: any = setInterval(() => setScrollY(window.scrollY),100)
+
+  useEffect(() => {
+    return () => clearInterval(verifyY)
+  }, [])
+
   return (
-    <header className={style.header_container}>
-      <h1>Breno</h1>
-      <nav className={style.header_nav}>
+    <header
+      style={{
+        backgroundColor: scrollY > 150 ? 'rgba(0,0,0,0.8)' : 'rgba(0,0,0,0.0)',
+        padding: scrollY > 150 ? '10px' : '20px' ,
+      }}
+      className={style.header_container}
+    >
+      <h1
+      style={{
+        marginTop: scrollY > 150 ? '0rem' : '1rem',
+      }}
+      >Breno</h1>
+      <nav
+      className={style.header_nav}
+      style={{
+        marginTop: scrollY > 150 ? '0rem' : '1rem',
+      }}
+      >
         <Link href={"/"}>
-          <span>Home</span>
+          <span
+            style={{
+              marginTop: scrollY > 150 ? '1rem' : '1.4rem',
+            }}
+          >Home</span>
         </Link>
         <div className={style.divisor} />
         <Link href={"/"}>
-          <span>Projetos</span>
+          <span
+            style={{
+              marginTop: scrollY > 150 ? '1rem' : '1.4rem',
+            }}
+          >Projetos</span>
         </Link>
         <div className={style.divisor} />
         <Link href={"/"}>
-          <span>Sobre</span>
+          <span
+            style={{
+              marginTop: scrollY > 150 ? '1rem' : '1.4rem',
+            }}
+          >Sobre</span>
         </Link>
       </nav>
       <div className={style.bottom_divisor} />

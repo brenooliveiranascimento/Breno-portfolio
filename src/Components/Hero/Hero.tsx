bbab83019d07d37cf98a81d34b57dcee7f82049d
Breno Nascimento <satsukebare23@gmail.com>
2022-08-15 22:41:25 -0300

    add my name in Hero

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import style from './hero.module.scss'
 
 function Hero() {
   return (
     <section className={style.heroContainer}>
       <section className={style.hero_darkness}>
-        <h1>Hero!!</h1>
-        <span>aaa</span>
+        <section className={style.centralize_content}>
+          <h2>
+            Olá me chamo Breno e sou desenvolvedor
+          </h2>
+          <TypeAnimation
+            sequence={['FrontEnd', 3000, 'Mobile', 3000]}
+            style={{ fontSize: '2.8em' }}
+            wrapper="h1"
+            repeat={Infinity}
+          />
+
+        </section>
       </section>
     </section>
   )

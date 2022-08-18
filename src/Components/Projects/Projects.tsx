7212840c55c9c02b418b7131a57fc36db27fa896
Breno Nascimento <satsukebare23@gmail.com>
2022-08-18 00:03:42 -0300

    add drawingStation slide and redux wallet slide

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -3,11 +3,41 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import DrawingWeb1 from '../../images/Projects/DrawingStation/drawing1.png'
 import DrawingWeb2 from '../../images/Projects/DrawingStation/drawing2.png'
 import DrawingWeb3 from '../../images/Projects/DrawingStation/drawing3.png'
+import ReduxWallet1 from '../../images/Projects/ReduxWallet/wallet1.png'
+import ReduxWallet2 from '../../images/Projects/ReduxWallet/wallet2.png'
+
 import 'swiper/css';
 import SwiperCore, { Autoplay } from 'swiper';
 SwiperCore.use([Autoplay]);
 import style from './Projects.module.scss'
 import Image from 'next/image';
+
+const allProjects = {
+  drawingStationWeb: {
+    name: 'DrawingStation',
+    tecnologias: ['React', 'Typescript', 'Redux'],
+    images: [
+      DrawingWeb1,
+      DrawingWeb3,
+      DrawingWeb2,
+    ],
+    description: '',
+    gitHub: '',
+    goProject:''
+  },
+  reduxWallet: {
+    name: 'ReduxWallet',
+    tecnologias: ['React', 'Typescript', 'Redux'],
+    images: [
+      ReduxWallet1,
+      ReduxWallet2,
+    ],
+    description: '',
+    gitHub: '',
+    goProject:''
+  },
+}
+
 const images = [
   DrawingWeb1,
   DrawingWeb3,
@@ -20,17 +50,22 @@ import "swiper/css/navigation";
 
 // import required modules
 import { Pagination, Navigation } from "swiper";
+import { redirect } from 'next/dist/server/api-utils';
 
 
 function Projects() {
   return (
-    <main
-      className={style.project_container}
-    >
-      <section
-        className={style.projectAligns}
-      >
-        <ItemCard />
+    <main className={style.project_container} >
+      <section className={style.projectAligns}>
+          {
+            Object.keys(allProjects).map((project) => (
+              <section className={style.card}>
+                <ItemCard photos={allProjects[project].images} />
+                <h1>{allProjects[project].name}</h1>
+              </section>
+            ))
+          }
+
       </section>
     </main>
   );
@@ -39,7 +74,7 @@ function Projects() {
 export default Projects;
 
 
-const ItemCard = () => {
+const ItemCard = ({photos}: any) => {
   return (
     <Swiper
     pagination={true}
@@ -47,7 +82,7 @@ const ItemCard = () => {
       width: 800
     }}
     autoplay={{
-      delay: 3000,
+      delay: 6000,
       disableOnInteraction: false
   }}
     loop
@@ -55,7 +90,7 @@ const ItemCard = () => {
     modules={[Pagination, Navigation]}
     className={style.mySwiper}
   >
-      {images.map((image, i) => (
+      {photos.map((image, i) => (
         <SwiperSlide
          key={i} className="">
           <Image
